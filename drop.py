31aacb9eda48f26a8ef14a0a2f5e166db760b3ff
mag5323 <mag5323@gmail.com>
2014-12-29 16:02:17 +0800

    wrap render part as toJson function

--- drop.py
+++ drop.py
@@ -1,24 +1,29 @@
 from snap import *
 import json
 
-File = "data/facebook/698.edges"
-UGraph = LoadEdgeList(PUNGraph, File, 0, 1, " ")
+def toJson(UGraph, Json):
+    Nodes = []
+    Edges = []
 
-Nodes = []
-Edges = []
+    for NI in UGraph.Nodes():
+        Node = {}
+        Node['name'] = NI.GetId()
+        Nodes.append(Node)
 
-for NI in UGraph.Nodes():
-    Node = {}
-    Node['name'] = NI.GetId()
-    Nodes.append(Node)
+        for i in range(NI.GetDeg()):
+            Edge = {}
+            Edge['source'] = NI.GetId()
+            Edge['target'] = NI.GetNbrNId(i)
+            Edges.append(Edge)
 
-    for i in range(NI.GetDeg()):
-        Edge = {}
-        Edge['source'] = NI.GetId()
-        Edge['target'] = NI.GetNbrNId(i)
-        Edges.append(Edge)
+    Data = {'Nodes': Nodes, 'Edges': Edges}
 
-Data = {'Nodes': Nodes, 'Edges': Edges}
+    with open(Json, 'w') as outfile:
+        json.dump(Data, outfile, sort_keys=True, indent=4, separators=(',', ': '))
 
-with open('data.json', 'w') as outfile:
-    json.dump(Data, outfile, sort_keys=True, indent=4, separators=(',', ': '))
+    return None
+
+File = 'data/facebook/698.edges'
+UGraph = LoadEdgeList(PUNGraph, File, 0, 1, ' ')
+
+toJson(UGraph, 'result.json')
