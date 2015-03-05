954b1488c2ca8e7cad2aecba082a6a0a1a15ef02
mag5323 <mag5323@gmail.com>
2015-03-05 17:48:36 +0800

    init new-alog

---
+++ func.py
@@ -0,0 +1,11 @@
+def toJson(G):
+    Dis = {'Edges': [], 'Nodes': []}
+    for N in G.Nodes():
+        Dis['Nodes'].append({'name': N.GetId()})
+    for E in G.Edges():
+        Dis['Edges'].append({'source': E.GetSrcNId(), 'target': E.GetDstNId()})
+    with open('output.json', 'w') as outfile:
+        json.dump(Dis, outfile, indent = 2)
+
+def dumpList(L):
+    return ', '.join(map(str, L))
---
+++ mag.py
@@ -0,0 +1,61 @@
+from snap import *
+from func import *
+from operator import itemgetter
+import json
+
+G = TUNGraph.New()
+
+for N in range(1, 7):
+    G.AddNode(N)
+
+G.AddEdge(1, 2)
+G.AddEdge(1, 3)
+G.AddEdge(2, 3)
+G.AddEdge(2, 4)
+G.AddEdge(3, 5)
+G.AddEdge(4, 5)
+G.AddEdge(5, 6)
+
+Centr = []
+Neighbors = {}
+for NI in G.Nodes():
+    NId = NI.GetId()
+    Centr.append({'id': NId, 'centr': GetDegreeCentr(G, NId)})
+
+    TempNeighbor = []
+    for Id in NI.GetOutEdges():
+        TempNeighbor.append(Id)
+    Neighbors[NId] = TempNeighbor
+
+Centr.sort(key=lambda x: x['centr'])
+
+Max = Centr.pop()['id']
+NIdV = TIntV()
+NIdV.Add(Max)
+for N in Neighbors[Max]:
+    NIdV.Add(N)
+
+SubGraph = GetSubGraph(G, NIdV)
+G.Dump()
+SubGraph.Dump()
+
+#print '\nBetweenness Centrality'
+#Nodes = TIntFltH()
+#Edges = TIntPrFltH()
+#GetBetweennessCentr(G, Nodes, Edges, 1.0)
+#for node in Nodes:
+#    print "node: %d centrality: %f" % (node, Nodes[node])
+#
+#print '\nCloseness Centrality'
+#for NI in G.Nodes():
+#    CloseCentr = GetClosenessCentr(G, NI.GetId())
+#    print "node: %d centrality: %f" % (NI.GetId(), CloseCentr)
+#
+#print '\nFarness Centrality'
+#for NI in G.Nodes():
+#    FarCentr = GetFarnessCentr(G, NI.GetId())
+#    print "node: %d centrality: %f" % (NI.GetId(), FarCentr)
+#
+#print '\nNode Centrality'
+#for NI in G.Nodes():
+#    print NI.GetId(), GetNodeEcc(G, NI.GetId(), False)
